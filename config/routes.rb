a553cfd1e871525565dcf7b1b421213b92217e87
bambi teague <teeegsa@gmail.com>
2021-04-30 23:29:50 -0500

    add resource to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :costumes
   resources :globalusercostumes
   resources :globalusers
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
