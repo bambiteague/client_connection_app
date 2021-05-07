9ba21ca7b508dc9063e9f7ab7a2e044bc0f1dd05
bambi teague <teeegsa@gmail.com>
2021-05-07 05:01:49 -0500

    validation messages added to costume model

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,7 +2,8 @@ class Costume < ApplicationRecord
   has_many :commissions
   has_many :globalusers, through: :commissions
 
-  validates :title, presence: true
+  validates :title, presence: {message: "Don't forget to add a title in order to submit your costume commission request!"}
   validates :type, inclusion: {in: %w(head mini-partial 3/4-partial fullsuit-digigrade fullsuit-plantigrade hand-paws feet-paws)}
-  validates :reference_sheet, presence: true
+        #still need to add a validation message about this data. May need to change validation type for this element after I create the drop down for it's data
+  validates :reference_sheet, presence: {message: "A reference sheet is required to submit your character for costume fabrication!"}
 end
