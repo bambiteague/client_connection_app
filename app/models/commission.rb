5703e3378f8b08a76db3a1241cbf94bf32e0be1f
bambi teague <teeegsa@gmail.com>
2021-05-07 05:02:49 -0500

    validations for commission model

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -2,5 +2,5 @@ class Commission < ApplicationRecord
   belongs_to :globaluser
   belongs_to :costume
 
-  validates :costume_status, presence: true
+  validates :costume_status, presence: {message: "Please update the commission's status"}
 end
