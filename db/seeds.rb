db4e76a2df790ee09138599efb5f557a6990f557
bambi teague <teeegsa@gmail.com>
2021-05-12 03:19:34 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,36 @@
+#I'm not seeding first and last names since they aren't required for any functions in my app at this point in developement
 
 # Clients ---->
 
 Globaluser.create(username: "RadiantRose", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "radiantrose@gmail.com")
 
-Globaluser.create(username: "Grinsome", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "Grinsome", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "grinsome@gmail.com")
 
-Globaluser.create(username: "CactiMutt", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "CactiMutt", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "cactimutt@gmail.com")
 
-Globaluser.create(username: "smolfuzz", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "smolfuzz", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "smolfuzz")
 
-Globaluser.create(username: "ferntography", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "ferntography", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "ferntography@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "taxigoesbeep", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "taxigoesbeep@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "sillyjish", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "sillyjish@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "dragoncatclem", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "dragoncatclem@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "silverthefur", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "silverfur@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "hydrosdagazelle", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "gazelleguy@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "mangowolfworks", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "mangowolf@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "mothsprout", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "mothsprout@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "auguste.artem", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "augustarts@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "fnook_bunny", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "fnookbun@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
+Globaluser.create(username: "Juicyb0nes", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "juicybones@gmail.com")
 
 # Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
 
@@ -41,11 +42,11 @@ Globaluser.create(username: "GoatyGoods", first_name: "", last_name: "", client:
 
 Globaluser.create(username: "BeautyoftheBass", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "bass@gmail.com")
 
-Globaluser.create(username: "axosaur", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "axosaur@gmail.com")
+Globaluser.create(username: "red_crocodile_creations", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "RCC@gmail.com")
 
 Globaluser.create(username: "FaunaFur", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "fauna@gmail.com")
 
-Globaluser.create(username: "sunset.tiger", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "sunset.tiger@gmail.com")
+Globaluser.create(username: "TigerTantrumStudios", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "sunset.tiger@gmail.com")
 
 Globaluser.create(username: "blackcheetah_studios", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "bcstudios@gmail.com")
 
@@ -64,28 +65,40 @@ Globaluser.create(username: "GreyMooseStudios", first_name: "", last_name: "", c
 
 # Costumes commissioned by clients ---->
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Red", style: "", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/122426855_184842113098471_6345229058619944909_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=107&_nc_ohc=C5T9IWTCbmkAX8kRO2z&edm=AP_V10EBAAAA&ccb=7-4&oh=6bc1484f2a644034ea85607f586e2e33&oe=60C2AD90&_nc_sid=4f375e", globaluser_id: 1)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Mellow pt.1", style: "", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/126777087_2779829159013386_5486781525292920783_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=102&_nc_ohc=CiTSrR4nI6YAX_LhppA&edm=AP_V10EBAAAA&ccb=7-4&oh=ab5b29472389a87a8787cd8721ac6b6a&oe=60C040C1&_nc_sid=4f375e", globaluser_id: 2)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Mellow pt.2", style: "", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/126777087_2779829159013386_5486781525292920783_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=102&_nc_ohc=CiTSrR4nI6YAX_LhppA&edm=AP_V10EBAAAA&ccb=7-4&oh=ab5b29472389a87a8787cd8721ac6b6a&oe=60C040C1&_nc_sid=4f375e", globaluser_id: 2)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Daku", style: "", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/183781861_905785203317104_6530528864015473886_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=104&_nc_ohc=3ZI1caYA6YMAX_hlrhE&edm=AP_V10EBAAAA&ccb=7-4&oh=6887a2bc036dfdc3724e400065132716&oe=60C1A274&_nc_sid=4f375e", globaluser_id: 3)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Pilot", style: "", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/180464611_126475366184787_5841351577333348249_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=103&_nc_ohc=EnTxKADtQz4AX-iNlFe&edm=AP_V10EBAAAA&ccb=7-4&oh=4d35dd5590651e9091c3e27b992e7ab4&oe=60C0DF52&_nc_sid=4f375e", globaluser_id: 4)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Mosh", style: "", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/179810389_3995641007140619_4674894322310137204_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=111&_nc_ohc=SZFXhdTmWE0AX_-mkn1&tn=DoJRoDwZ55Bi1gG2&edm=APU89FABAAAA&ccb=7-4&oh=c1a6e37c2cfea8dd53ef6991450d55cd&oe=60C2922B&_nc_sid=86f79a", globaluser_id: 5)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Lavender pt.1", style: "head", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/177338479_488416345539503_4013188873248309621_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=111&_nc_ohc=Y3l606bCacEAX-318ru&edm=AP_V10EBAAAA&ccb=7-4&oh=8fd89c896f6d0d9c9b41a9943eb74430&oe=60BFC871&_nc_sid=4f375e", globaluser_id: 6)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Lavender pt.2", style: "tail", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/177338479_488416345539503_4013188873248309621_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=111&_nc_ohc=Y3l606bCacEAX-318ru&edm=AP_V10EBAAAA&ccb=7-4&oh=8fd89c896f6d0d9c9b41a9943eb74430&oe=60BFC871&_nc_sid=4f375e", globaluser_id: 6)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Thunderpop", style: "fullsuit-digigrade", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/175528500_174798581159560_2255735952896821223_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=104&_nc_ohc=7bdZtu3lP6QAX9s55_A&edm=AP_V10EBAAAA&ccb=7-4&oh=73fc284f1807a6cb24efd4d634bb8a6f&oe=60C1032B&_nc_sid=4f375e", globaluser_id: 7)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Basil - opt tail", style: "tail", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/175563620_1597258463818363_3955844826933080038_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=107&_nc_ohc=ST9s8N3_wM8AX8d0BeC&edm=AP_V10EBAAAA&ccb=7-4&oh=7527f2598898e57d028dbbba1e53be85&oe=60C26688&_nc_sid=4f375e", globaluser_id: 8)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "Blue Cat", style: "feet paws", reference_sheet: "https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/e35/174481031_134600658629704_2368677906280307800_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=104&_nc_ohc=sz2sEjBz3k4AX8mVN1y&edm=AP_V10EBAAAA&ccb=7-4&oh=88598b728dac99495399d664f190b58e&oe=60C1C1C4&_nc_sid=4f375e", globaluser_id: 9)
 
-Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
+Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: 10)
+
+Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: 11)
+
+Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: 12)
+
+Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: 13)
+
+Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: 14)
+
+Costume.create(title: "Peony pt.1", style: "head", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/170408204_5524711350887187_5342202042721084667_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=103&_nc_ohc=JWupsi711BMAX9sVHoL&edm=AP_V10EBAAAA&ccb=7-4&oh=0121b53b4d4af9711042551a3c64e7e3&oe=60C26A93&_nc_sid=4f375e", globaluser_id: 15)
+
+Costume.create(title: "Peony pt. 2", style: "hand paws", reference_sheet: "https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/170408204_5524711350887187_5342202042721084667_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=103&_nc_ohc=JWupsi711BMAX9sVHoL&edm=AP_V10EBAAAA&ccb=7-4&oh=0121b53b4d4af9711042551a3c64e7e3&oe=60C26A93&_nc_sid=4f375e", globaluser_id: 15)
 
 # Costume.create(title: "", style: "", reference_sheet: "", globaluser_id: )
