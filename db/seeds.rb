82ed9d88c3087e942b6cb156fd52ad87920636f9
bambi teague <teeegsa@gmail.com>
2021-05-12 02:38:09 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 
 # Clients ---->
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "RadiantRose", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "radiantrose@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "Grinsome", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "CactiMutt", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "smolfuzz", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "ferntography", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "", first_name: "", last_name: "", client: true, designer: false,  password: "password", email: "")
 
 # Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
 
@@ -37,25 +37,25 @@ Globaluser.create(username: "", first_name: "", last_name: "", client:  , design
 
 # Designer ---->
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "GoatyGoods", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "goatygoods@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "BeautyoftheBass", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "bass@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "axosaur", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "axosaur@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "FaunaFur", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "fauna@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "sunset.tiger", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "sunset.tiger@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "blackcheetah_studios", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "bcstudios@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "Gretchka6666", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "deathsmile@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "the.paw.bakery", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "pawbakery@gmail.com")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "NormansNeedleworks", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "nncontact@gmail")
 
-Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "" , email: "")
+Globaluser.create(username: "GreyMooseStudios", first_name: "", last_name: "", client: false, designer: true, password: "password", email: "greymoosestudios@gmail.com")
 
 # Globaluser.create(username: "", first_name: "", last_name: "", client:  , designer: , password: "", email: "")
 
