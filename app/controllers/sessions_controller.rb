615fe515ffc9fb2be9ad67ec720d6b1a895ee51c
bambi teague <teeegsa@gmail.com>
2021-05-12 00:47:32 -0500

    deleted and reinstalled sessions controller (accident)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,23 @@ class SessionsController < ApplicationController
       redirect_to globaluser_path(user)
     else
       flash.now[:message] = "Incorrect login information"
-    
+      #won't want to re-render the new page with prepopulated information
       redirect_to '/login'
     end
   end
+
+  def omniauth
+    #processing the response we recieved from Google and either:
+    #create a new user 
+    #or 
+    #log in an existing user
+    user = Globaluser.from_omniauth(request.env['omniauth.auth'])
+    if user.valid?
+      session[:globaluser_id] = user.id
+      redirect_to profile_path(user)
+    else
+      redirect_to '/login'
+    end
+  end
+  
 end
